365a3215086ee36432faebc34da14365b4ebd7dd
Rafael Moraes <rafael.msantos@sptech.school>
2023-04-27 13:27:22 -0300

    acrescentando comentario aos posts

--- workshopmongo/src/main/java/com/rafaelfelipe/workshopmongo/domain/Post.java
+++ workshopmongo/src/main/java/com/rafaelfelipe/workshopmongo/domain/Post.java
@@ -2,11 +2,13 @@ package com.rafaelfelipe.workshopmongo.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.rafaelfelipe.workshopmongo.dto.AuthorDTO;
+import com.rafaelfelipe.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -70,6 +72,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
