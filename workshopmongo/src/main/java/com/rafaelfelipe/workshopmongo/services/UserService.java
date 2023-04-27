81351fac702793c4a0dc7a15badd58f7d50265a9
Rafael Moraes <rafael.msantos@sptech.school>
2023-04-27 11:08:35 -0300

    obtendo usuario pelo id

--- workshopmongo/src/main/java/com/rafaelfelipe/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/rafaelfelipe/workshopmongo/services/UserService.java
@@ -1,20 +1,25 @@
 package com.rafaelfelipe.workshopmongo.services;
-
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafaelfelipe.workshopmongo.domain.User;
 import com.rafaelfelipe.workshopmongo.repository.UserRepository;
+import com.rafaelfelipe.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-
 	@Autowired
 	private UserRepository repo;
-
+	
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
