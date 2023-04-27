9e61c488be7f2655fdcd284da30ef57d0949152c
Rafael Moraes <rafael.msantos@sptech.school>
2023-04-27 11:19:54 -0300

    insercao de usuario com post

--- workshopmongo/src/main/java/com/rafaelfelipe/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/rafaelfelipe/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafaelfelipe.workshopmongo.domain.User;
+import com.rafaelfelipe.workshopmongo.dto.UserDTO;
 import com.rafaelfelipe.workshopmongo.repository.UserRepository;
 import com.rafaelfelipe.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,4 +23,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
